20a101dfa5da5b588f1e77f4069409880d8c23b4
jjaneyxx <hmyeonge@gmail.com>
2025-01-24 23:11:00 +0900

    fix: axios.js에서 자동 포맷팅으로 작은 따옴표로 변경

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,17 +1,17 @@
-import axios from "axios";
+import axios from 'axios';
 
 export const apiClient = axios.create({
-  baseURL: "https://rolling-api.vercel.app",
+  baseURL: 'https://rolling-api.vercel.app',
   headers: {
-    "Content-Type": "application/json",
+    'Content-Type': 'application/json',
   },
 });
 
 const apiRoutes = {
   // Background Images
-  getBackgroundImages: "/background-images/",
+  getBackgroundImages: '/background-images/',
   // Profile Images
-  getProfileImages: "/profile-images/",
+  getProfileImages: '/profile-images/',
   // Messages
   getMessages: (team, messageId) => `/${team}/messages/${messageId}/`,
   putMessage: (team, messageId) => `/${team}/messages/${messageId}/`,
@@ -22,14 +22,10 @@ const apiRoutes = {
   postRecipient: (team) => `/${team}/recipients/`,
   getRecipient: (team, recipientId) => `/${team}/recipients/${recipientId}`,
   deleteRecipient: (team, recipientId) => `/${team}/recipients/${recipientId}`,
-  getRecipientMessages: (team, recipientId) =>
-    `/${team}/recipients/${recipientId}/messages/`,
-  postRecipientMessage: (team, recipientId) =>
-    `/${team}/recipients/${recipientId}/messages/`,
-  getRecipientReactions: (team, recipientId) =>
-    `/${team}/recipients/${recipientId}/reactions/`,
-  postRecipientReaction: (team, recipientId) =>
-    `/${team}/recipients/${recipientId}/reactions/`,
+  getRecipientMessages: (team, recipientId) => `/${team}/recipients/${recipientId}/messages/`,
+  postRecipientMessage: (team, recipientId) => `/${team}/recipients/${recipientId}/messages/`,
+  getRecipientReactions: (team, recipientId) => `/${team}/recipients/${recipientId}/reactions/`,
+  postRecipientReaction: (team, recipientId) => `/${team}/recipients/${recipientId}/reactions/`,
 };
 
 const api = {
@@ -40,21 +36,16 @@ const api = {
   getProfileImages: () => apiClient.get(apiRoutes.getProfileImages),
 
   // Messages
-  getMessages: (team, messageId) =>
-    apiClient.get(apiRoutes.getMessages(team, messageId)),
-  putMessage: (team, messageId, data) =>
-    apiClient.put(apiRoutes.putMessage(team, messageId), data),
+  getMessages: (team, messageId) => apiClient.get(apiRoutes.getMessages(team, messageId)),
+  putMessage: (team, messageId, data) => apiClient.put(apiRoutes.putMessage(team, messageId), data),
   patchMessage: (team, messageId, data) =>
     apiClient.patch(apiRoutes.patchMessage(team, messageId), data),
-  deleteMessage: (team, messageId) =>
-    apiClient.delete(apiRoutes.deleteMessage(team, messageId)),
+  deleteMessage: (team, messageId) => apiClient.delete(apiRoutes.deleteMessage(team, messageId)),
 
   // Recipients
   getRecipients: (team) => apiClient.get(apiRoutes.getRecipients(team)),
-  postRecipient: (team, data) =>
-    apiClient.post(apiRoutes.postRecipient(team), data),
-  getRecipient: (team, recipientId) =>
-    apiClient.get(apiRoutes.getRecipient(team, recipientId)),
+  postRecipient: (team, data) => apiClient.post(apiRoutes.postRecipient(team), data),
+  getRecipient: (team, recipientId) => apiClient.get(apiRoutes.getRecipient(team, recipientId)),
   deleteRecipient: (team, recipientId) =>
     apiClient.delete(apiRoutes.deleteRecipient(team, recipientId)),
 
