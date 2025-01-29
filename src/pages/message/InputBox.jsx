f01d4ee9114d7dbcdfb4308c41fe37352560e1a3
jjaneyxx <hmyeonge@gmail.com>
2025-01-29 16:50:27 +0900

    feat: 입력값 검증 및 에러 메시지 표시 추가

--- src/pages/message/InputBox.jsx
+++ src/pages/message/InputBox.jsx
@@ -1,15 +1,18 @@
-const InputBox = ({ value, onChange }) => {
+const InputBox = ({ value, onChange, onBlur, isError }) => {
   return (
-    <input
-      type="text"
-      name="sender"
-      id="from-input"
-      value={value}
-      onChange={onChange}
-      className="py-3 px-4 h-[50px] rounded-lg border border-[#CCCCCC] "
-      placeholder="이름을 입력해 주세요."
-      maxLength="10"
-    />
+    <>
+      <input
+        type="text"
+        name="sender"
+        id="from-input"
+        value={value}
+        onChange={onChange}
+        onBlur={onBlur}
+        className={`py-3 px-4 h-[50px] rounded-lg border ${isError ? 'border-[#DC3A3A]' : 'border-[#CCCCCC]'}`}
+        maxLength="10"
+      />
+      {isError && <p className="text-[#DC3A3A]">값을 입력해주세요.</p>}
+    </>
   );
 };
 export default InputBox;
--- src/pages/message/index.jsx
+++ src/pages/message/index.jsx
@@ -1,11 +1,22 @@
 import './styles.css';
 import InputBox from './InputBox';
 import { useState } from 'react';
+
 const Message = () => {
   const [sender, setSender] = useState('');
+  const [isError, setIsError] = useState(false);
 
   const handleInputChange = (e) => {
     setSender(e.target.value); // 공백 제거 후 상태 업데이트
+    console.log(e.target.value);
+    setIsError(false);
+  };
+
+  const handleOnBlur = () => {
+    if (sender === '') {
+      console.log('focusout');
+      setIsError(true);
+    }
   };
 
   return (
@@ -13,8 +24,14 @@ const Message = () => {
       <label htmlFor="from-input" className="text-24-bold mb-3">
         From.
       </label>
-      <InputBox value={sender} onChange={handleInputChange} />
+      <InputBox
+        value={sender}
+        onChange={handleInputChange}
+        onBlur={handleOnBlur}
+        isError={isError}
+      />
     </form>
   );
 };
+
 export default Message;
