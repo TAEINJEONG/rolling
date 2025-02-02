49929abda9172bdae300da66dab58e5d6d9cf549
ARON-Y <arar0811@gmail.com>
2025-02-02 21:39:38 +0900

    style: 버튼 스타일 개선

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,18 @@ import Layout from './pages/Layout';
 import Colors from './pages/Colors';
 import Typography from './pages/Typography';
 import './index.css';
+import Button from './components/common/button';
+import addemoji from './assets/images/add-emoji.svg';
 
 function App() {
   return (
     <BrowserRouter>
+      <Button variant="primary" size="xl" disabled={false}>
+        구경해보기
+      </Button>
+      <Button variant="outlined" size="md" disabled={false} icon={addemoji}>
+        추가
+      </Button>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route path="colors" element={<Colors />} />
