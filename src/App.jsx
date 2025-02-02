f89a0a2e1a90ae1328b05b26fa20d14291fb89de
hdh12191 <hdh12191@naver.com>
2025-02-02 13:29:48 +0900

    feat : 기본 레이아웃 구성 및  router 작업 완료

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,9 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Layout from './pages/Layout';
-import Colors from './pages/Colors';
-import Typography from './pages/Typography';
 import './index.css';
+import AppRouter from './routes';
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route path="colors" element={<Colors />} />
-          <Route path="typography" element={<Typography />} />
-          {/* 라우터 내용 채우기 */}
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  );
+  return <AppRouter />;
 }
 
 export default App;
