b45f87154d39c7bdf39e23879b6dd3e3faf9f1bb
ARON-Y <arar0811@gmail.com>
2025-02-03 15:24:11 +0900

    feat : 롤링페이퍼 버튼 조건부 노출 및 링크 연결

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
@@ -7,16 +6,3 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   plugins: [react(), tailwindcss()],
 });
-=======
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss()
-  ],
-})
->>>>>>> 604993c6f7a4312cbab1de9a61a77938939f2383
