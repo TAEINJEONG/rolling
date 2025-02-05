b225794a9113b3499b8f361423d1af0254fda522
hdh12191 <hdh12191@naver.com>
2025-02-05 19:45:09 +0900

    feat : 캐러셀 컴포넌트 구조 개선 및 스타일링 업데이트

--- src/pages/list/components/RollingPaperCard.jsx
+++ src/pages/list/components/RollingPaperCard.jsx
@@ -1,7 +1,7 @@
 const RollingPaperCard = ({ paper }) => {
   return (
     <div
-      className="w-[208px] h-[232px] xl:w-[275px] xl:h-[260px] bg-amber-900 rounded-2xl transition-all duration-150"
+      className="w-[208px] h-[232px] md:w-[275px] md:h-[260px] bg-amber-900 rounded-2xl transition-all duration-150"
       key={paper.id}
     >
       {paper.name}
