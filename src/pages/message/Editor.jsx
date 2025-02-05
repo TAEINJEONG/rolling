45a645d763652e18567416ca1c652e18dd48dad2
jjaneyxx <hmyeonge@gmail.com>
2025-02-05 15:12:02 +0900

    fix: 폰트 변경 유지 및 Quill 내부 동작으로 인한 줄바꿈 제거
    - 에디터에 내용을 작성 중 폰트 변경 시, 이후 작성되는 내용도 해당 폰트가
      적용되도록 구현
    - Quill 내부 동작으로 인해 생기는 불필요한 줄바꿈 제거

--- src/pages/message/Editor.jsx
+++ src/pages/message/Editor.jsx
@@ -8,10 +8,11 @@ const Font = ReactQuill.Quill.import('formats/font');
 Font.whitelist = ['NotoSans', 'Pretendard', 'NanumMyeongjo', 'NanumSonPyeonJiCe'];
 ReactQuill.Quill.register(Font, true);
 
+// 폰트 이름을 Quill 클래스 형식으로 바로 변환
 const fontMap = {
-  'Noto Sans': 'Noto Sans',
+  'Noto Sans': 'NotoSans',
   Pretendard: 'Pretendard',
-  나눔명조: 'Nanum Myeongjo',
+  나눔명조: 'NanumMyeongjo',
   '나눔손글씨 손편지체': 'NanumSonPyeonJiCe',
 };
 
@@ -26,27 +27,33 @@ const modules = {
 };
 
 const Editor = ({ editorContent, setEditorContent, selectedFont }) => {
-  const editorRef = useRef(null); // 에디터에 접근하기 위한 ref
+  const editorRef = useRef(null);
 
   useEffect(() => {
     if (editorRef.current) {
       const editor = editorRef.current.getEditor();
-      editor.removeFormat(0, editor.getLength()); // 에디터 텍스트 포맷 초기화
-      const quillFont = fontMap[selectedFont]; // Select 폼 option -> quillFont 매핑
-      editor.formatText(0, editor.getLength(), 'font', quillFont.split(' ').join('')); // 폰트 적용
+      editor.formatText(0, editor.getLength(), 'font', fontMap[selectedFont]); // 폰트 적용
     }
   }, [selectedFont]);
 
   const handleEditorChange = (content) => {
-    let modifiedContent = content;
+    let modifiedContent = content.trim();
+    const fontClass = 'ql-font-' + fontMap[selectedFont]; // 선택된 폰트 클래스 이름 생성
 
-    // 폰트가 선택되지 않은 경우 <span class="ql-font-NotoSans"> 를 자동 추가
-    if (!content.includes('ql-font')) {
-      modifiedContent = content.replace(
-        /<p>(.*?)<\/p>/g,
-        '<p><span class="ql-font-NotoSans">$1</span></p>'
-      );
-    }
+    modifiedContent = modifiedContent.replace(/<p>([\s\S]*?)<\/p>/g, (_, innerContent) => {
+      // Quill 내부 동작으로 생기는 불필요한 줄바꿈 제거
+      const textWithoutBr = innerContent.replace(/<br\s*\/?>/g, '').trim();
+      if (textWithoutBr === '') {
+        return '';
+      }
+      // 현재 선택된 폰트 적용
+      if (/^\s*<span class="ql-font-/.test(innerContent)) {
+        return `<p>${innerContent}</p>`;
+      } else {
+        return `<p><span class="${fontClass}">${innerContent}</span></p>`;
+      }
+    });
+
     setEditorContent(modifiedContent);
   };
 
