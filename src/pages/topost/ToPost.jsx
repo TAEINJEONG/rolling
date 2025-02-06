8d0d2544d600e4aa98c69ee37da1e47e8d00e60b
rover1523 <wdd152323@gmail.com>
2025-02-06 13:50:44 +0900

    fix: PR 리뷰 반영 및 수정 사항 적용

--- src/pages/topost/ToPost.jsx
+++ src/pages/topost/ToPost.jsx
@@ -1,45 +1,51 @@
-import { useState, useEffect } from "react";
-import api from "../api/axios";
+import React from 'react';
+import { useState, useEffect } from 'react';
+import api from '../api/axios';
+import clsx from 'clsx';
+import selectedIcon from '../assets/images/selected.svg';
 
 const colors = [
-  { hex: "#FFE2AD", name: "beige" },
-  { hex: "#ECD9FF", name: "purple" },
-  { hex: "#B1E4FF", name: "blue" },
-  { hex: "#D0F5C3", name: "green" },
+  { hex: '#FFE2AD', name: 'beige' },
+  { hex: '#ECD9FF', name: 'purple' },
+  { hex: '#B1E4FF', name: 'blue' },
+  { hex: '#D0F5C3', name: 'green' },
 ];
 const borderStyle = {
-  error: "border-red-500 bg-red-50 focus:ring-2 focus:ring-red-500",
-  default: "border-gray-300 focus:ring-2 focus:ring-purple-500",
+  error: 'border-red-500 bg-red-50 focus:ring-2 focus:ring-red-500',
+  default: 'border-gray-300 focus:ring-2 focus:ring-purple-500',
 };
 const borderButton = {
-  selected: "bg-purple-500 text-white border-purple-500",
-  default: "border-purple-500 text-purple-500 hover:bg-purple-100",
+  selected: 'bg-purple-500 text-white border-purple-500',
+  default: 'border-purple-500 text-purple-500 hover:bg-purple-100',
 };
 function ToPost() {
-  const [post, setPost] = useState("");
+  const [post, setPost] = useState('');
   const [images, setImages] = useState([]);
-  const [selectedTab, setSelectedTab] = useState("color");
+  const [selectedTab, setSelectedTab] = useState('color');
   const [selectedColor, setSelectedColor] = useState(colors[0].hex);
   const [selectedImage, setSelectedImage] = useState(null);
   const [error, setError] = useState(false);
+  const [uploadedImages, setUploadedImages] = useState([]);
 
   //컬러 선택스타일
   const ColorOption = ({ color, selected, onClick }) => {
     return (
       <div
-        className="w-32 h-32 rounded-lg shadow-md cursor-pointer transition-all flex items-center justify-center"
+        className="relative w-32 h-32 rounded-lg shadow-md cursor-pointer transition-all flex items-center justify-center"
         style={{ backgroundColor: color }}
         onClick={() => onClick(color)}
       >
+        {/* 선택된 경우 체크 아이콘 표시 */}
         {selected && (
-          <div className="w-10 h-10 bg-gray-700 rounded-full flex items-center justify-center text-white">
-            √
+          <div className="absolute inset-0 flex items-center justify-center">
+            <div className="w-11 h-11 rounded-full flex items-center justify-center">
+              <img src={selectedIcon} alt="Check" />
+            </div>
           </div>
         )}
       </div>
     );
   };
-
   //컬러 선택
   const ColorSelector = ({ selectedColor, setSelectedColor }) => {
     return (
@@ -55,19 +61,27 @@ function ToPost() {
       </div>
     );
   };
+  //이미지 추가 옵션
+  const handleImageUpload = (event) => {
+    const files = event.target.files;
+    if (files.length > 0) {
+      const newImages = Array.from(files).map((file) => URL.createObjectURL(file));
+      setUploadedImages((prevImages) => [...prevImages, ...newImages]);
+    }
+  };
   //이미지 선택 스타일
-  const ImageOption = ({ src, selected, onClick }) => {
+  const SelectedOption = ({ src, selected, onClick }) => {
     return (
       <div
-        className={`w-32 h-32 rounded-lg shadow-md cursor-pointer transition-all flex items-center justify-center bg-cover bg-center ${
-          selected ? "border-4 border-purple-500" : "border-2 border-gray-300"
-        }`}
-        style={{ backgroundImage: `url(${src})` }}
+        className="relative w-[168px] h-[168px] rounded-lg cursor-pointer transition-all overflow-hidden"
         onClick={() => onClick(src)}
       >
+        <img src={src} alt="Selected Option" className="w-full h-full object-cover" />
         {selected && (
-          <div className="w-10 h-10 bg-gray-700 rounded-full flex items-center justify-center text-white">
-            √
+          <div className="absolute inset-0 flex items-center justify-center">
+            <div className="w-11 h-11 rounded-full flex items-center justify-center">
+              <img src={selectedIcon} alt="Check" />
+            </div>
           </div>
         )}
       </div>
@@ -79,7 +93,7 @@ function ToPost() {
   };
   //입력 유효성검사
   const handleBlur = () => {
-    if (post.trim() === "") {
+    if (post.trim() === '') {
       setError(true);
     }
   };
@@ -90,14 +104,14 @@ function ToPost() {
 
     const NEW_PAGE = {
       name: post,
-      backgroundColor: selectedTab === "color" ? selectedColor : "beige",
-      backgroundImageURL: selectedTab === "image" ? selectedImage : null,
+      backgroundColor: selectedTab === 'color' ? selectedColor : 'beige',
+      backgroundImageURL: selectedTab === 'image' ? selectedImage : null,
     };
 
     try {
-      const response = await api.postRecipient("13-2", NEW_PAGE);
+      const response = await api.postRecipient('13-2', NEW_PAGE);
     } catch (error) {
-      console.error("페이지 생성 실패:", error);
+      console.error('페이지 생성 실패:', error);
     }
   };
   //API를 통해 이미지 받아오기
@@ -107,7 +121,7 @@ function ToPost() {
         const response = await api.getBackgroundImages();
         setImages(response.data.imageUrls);
       } catch (error) {
-        console.error("이미지 로드 실패:", error);
+        console.error('이미지 로드 실패:', error);
       }
     };
     fetchImages();
@@ -129,66 +143,76 @@ function ToPost() {
             error ? borderStyle.error : borderStyle.default
           }`}
         />
-        {error && (
-          <p className="text-red-500 text-sm mt-2">이름을 입력해 주세요</p>
-        )}
+        {error && <p className="text-red-500 text-sm mt-2">이름을 입력해 주세요</p>}
         <div className="mt-8">
-          <h2 className="text-xl font-bold text-gray-900">
-            배경화면을 선택해 주세요
-          </h2>
-          <p className="text-gray-600 text-sm">
-            컬러를 선택하거나, 이미지를 선택할 수 있습니다.
-          </p>
+          <h2 className="text-xl font-bold text-gray-900">배경화면을 선택해 주세요</h2>
+          <p className="text-gray-600 text-sm">컬러를 선택하거나, 이미지를 선택할 수 있습니다.</p>
         </div>
         <div className="flex justify-start mt-4 space-x-0">
           <button
             type="button"
-            className={`px-6 py-2 text-sm font-bold border-2 rounded-lg transition-all ${
-              selectedTab === "color"
-                ? borderButton.selected
-                : borderButton.default
-            }`}
-            onClick={() => setSelectedTab("color")}
+            className={clsx(
+              'px-6 py-2 text-sm font-bold border-2 rounded-lg transition-all',
+              selectedTab === 'color' ? borderButton.selected : borderButton.default
+            )}
+            onClick={() => setSelectedTab('color')}
           >
-            색깔
+            컬러
           </button>
           <button
             type="button"
-            className={`px-6 py-2 text-sm font-bold border-2 rounded-lg transition-all ${
-              selectedTab === "image"
-                ? borderButton.selected
-                : borderButton.default
-            }`}
-            onClick={() => setSelectedTab("image")}
+            className={clsx(
+              'px-6 py-2 text-sm font-bold border-2 rounded-lg transition-all',
+              selectedTab === 'image' ? borderButton.selected : borderButton.default
+            )}
+            onClick={() => setSelectedTab('image')}
           >
             이미지
           </button>
         </div>
-        {selectedTab === "color" && (
-          <ColorSelector
-            selectedColor={selectedColor}
-            setSelectedColor={setSelectedColor}
-          />
+        {selectedTab === 'color' && (
+          <ColorSelector selectedColor={selectedColor} setSelectedColor={setSelectedColor} />
         )}
-        {selectedTab === "image" && (
-          <div className="grid grid-cols-3 gap-4 mt-4">
-            {images.length > 0 ? (
-              images.map((src) => (
-                <ImageOption
-                  key={src}
-                  src={src}
-                  selected={selectedImage === src}
-                  onClick={() => setSelectedImage(src)}
-                />
-              ))
-            ) : (
-              <p className="text-gray-500">이미지가 없습니다.</p>
-            )}
+        {selectedTab === 'image' && (
+          <div className="flex items-start gap-4 mt-4">
+            <label
+              htmlFor="file-upload"
+              className="flex items-center justify-center w-[150px] h-[150px] border border-gray-300 rounded-lg cursor-pointer shadow-sm hover:bg-gray-100 transition-all"
+            >
+              <span className="text-gray-500 text-sm">이미지 추가하기</span>
+            </label>
+            <input
+              id="file-upload"
+              type="file"
+              accept="image/*"
+              multiple
+              onChange={handleImageUpload}
+              className="hidden"
+            />
+            <div className="grid grid-cols-4 gap-4 w-full max-w-[700px] ">
+              {images.length > 0 &&
+                images.map((src) => (
+                  <SelectedOption
+                    key={src}
+                    src={src}
+                    selected={selectedImage === src}
+                    onClick={() => setSelectedImage(src)}
+                  />
+                ))}
+              {uploadedImages.length > 0 &&
+                uploadedImages.map((src) => (
+                  <SelectedOption
+                    key={src}
+                    src={src}
+                    selected={selectedImage === src}
+                    onClick={() => setSelectedImage(src)}
+                  />
+                ))}
+            </div>
           </div>
         )}
       </form>
     </div>
   );
 }
-
 export default ToPost;
