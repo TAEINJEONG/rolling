156f58c76c64992c4815ee1bfe35b61c0d66396e
ARON-Y <arar0811@gmail.com>
2025-02-06 14:15:57 +0900

    refactor : Button 컴포넌트에 fullWidth prop 추가

--- src/components/common/button/index.jsx
+++ src/components/common/button/index.jsx
@@ -1,22 +1,13 @@
 import { baseStyles, variants, sizes } from './buttonStyle';
 
-const Button = ({
-  children,
-  icon,
-  variant,
-  size,
-  disabled = false,
-  onClick,
-  fullWidth = false,
-}) => {
+const Button = ({ children, icon, variant, size, disabled = false, onClick, fullWidth }) => {
   const buttonStyles = `${baseStyles} ${sizes[size]} ${variants[variant]}`;
 
   return (
     <button
-      className={`${buttonStyles} flex items-center justify-center gap-[4px]`}
+      className={`${buttonStyles} flex items-center justify-center gap-[4px] ${fullWidth && 'w-full'}`}
       disabled={disabled}
       onClick={onClick}
-      style={{ width: fullWidth ? '100%' : 'auto' }}
     >
       {icon && <img src={icon} alt="emoji icon" />}
       {children}
