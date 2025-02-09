633feac5fe319171ae54a167dbd93334a650cfac
hdh12191 <hdh12191@naver.com>
2025-02-09 18:51:40 +0900

    feat : 랜딩페이지 모바일 화면 구현 및 구조 개선

--- src/pages/landing/index.jsx
+++ src/pages/landing/index.jsx
@@ -2,58 +2,45 @@ import React from 'react';
 import exampleEmojiImg from '../../assets/images/example_emoji.png';
 import exampleRollingImg from '../../assets/images/example_rolling.png';
 import Button from '../../components/common/button';
+import { useNavigate } from 'react-router-dom';
 
 const Landing = () => {
+  const navigate = useNavigate();
+
   return (
-    <div className="flex flex-col gap-4">
-      <div className="w-[1200px] pt-15 flex flex-col gap-[30px]">
-        <div className="h-[324px] flex justify-between items-center p-15 bg-[#F6F8FF]">
-          <div className="flex flex-col gap-4">
-            <div className="w-fit bg-purple-600 text-14-bold text-white px-3 py-1.5 rounded-[50px]">
-              Point. 01
-            </div>
-            <div className="flex flex-col gap-2">
-              <div>
-                <p className="text-24-bold">
-                  누구나 손쉽게, 온라인
-                  <br />
-                  롤링 페이퍼를 만들 수 있어요
-                </p>
-              </div>
-              <p className="w-fit text-18-regular text-gray-500">로그인 없이 자유롭게 만들어요.</p>
-            </div>
+    <div className="flex flex-col w-full gap-6 px-5 pt-[42px]">
+      <div className="flex flex-col px-6 pt-6 pb-[62px] gap-[50px] bg-[#F6F8FF] rounded-[20px] overflow-hidden">
+        <div className="flex flex-col gap-4">
+          <div className="bg-purple-600 text-14-bold text-white rounded-[50px] px-3 py-1 w-fit">
+            Point. 01
           </div>
-
-          <div className="w-[720px] h-[204px] px-10 py-5">
-            <img src={exampleRollingImg} alt="롤링 페이퍼 예시" />
+          <div className="flex flex-col gap-1">
+            <p className="text-18-bold text-[#181818]">
+              누구나 손쉽게, 온라인 롤링 페이퍼를 만들 수 있어요
+            </p>
+            <p className="text-15-regular text-[#555555]">로그인 없이 자유롭게 만들어요.</p>
           </div>
         </div>
-
-        <div className="h-[324px] flex items-center p-15 bg-[#F6F8FF]">
-          <div className="w-[720px] h-[204px] px-[125px]">
-            <img src={exampleEmojiImg} alt="이모지 예시" />
-          </div>
-          <div className="flex flex-col gap-4">
-            <div className="w-fit bg-purple-600 text-14-bold text-white px-3 py-1.5 rounded-[50px]">
-              Point. 02
-            </div>
-            <div className="flex flex-col gap-2">
-              <div>
-                <p className="text-24-bold">
-                  서로에게 이모지로 감정을
-                  <br />
-                  표현해보세요
-                </p>
-              </div>
-              <p className="w-fit text-18-regular text-gray-500">
-                롤링 페이퍼에 이모지를 추가할 수 있어요.
-              </p>
-            </div>
-          </div>
-        </div>
+        <img
+          src={exampleRollingImg}
+          alt="example_rolling"
+          className="min-w-[354px] max-[361px]:-ml-[45px] duration-300"
+        />
       </div>
-      <div className="flex justify-center py-6">
-        <Button variant="primary" size="lg">
+      <div className="flex flex-col px-6 pt-6 pb-[51px] gap-[48px] bg-[#F6F8FF] rounded-[20px] overflow-hidden">
+        <div className="flex flex-col gap-4">
+          <div className="bg-purple-600 text-14-bold text-white rounded-[50px] px-3 py-1 w-fit">
+            Point. 02
+          </div>
+          <div className="flex flex-col gap-1">
+            <p className="text-18-bold text-[#181818]">서로에게 이모지로 감정을 표현해보세요</p>
+            <p className="text-15-regular text-[#555555]">로그인없이 자유롭게 만들어요.</p>
+          </div>
+        </div>
+        <img src={exampleEmojiImg} alt="example_emoji" />
+      </div>
+      <div className="py-6 sticky bottom-0">
+        <Button variant="primary" size="lg" fullWidth>
           구경해보기
         </Button>
       </div>
