2365938de579b6a31485a1edc1f08b7cd424c6eb
rover1523 <wdd152323@gmail.com>
2025-02-10 23:01:46 +0900

    fix : 스타일 변경

--- src/pages/post/SelectedOption.jsx
+++ src/pages/post/SelectedOption.jsx
@@ -3,12 +3,12 @@ import selectedIcon from '../../assets/images/selected.svg';
 
 const SelectedOption = ({ src, selected, onClick }) => (
   <div
-    className="relative w-full h-full rounded-xl cursor-pointer transition-all overflow-hidden sm:w-32 sm:h-32 md:w-40 md:h-40 lg:w-60 lg:h-60 mb-0"
+    className="relative w-full aspect-square rounded-xl cursor-pointer overflow-hidden"
     onClick={() => onClick(src)}
   >
-    <img src={src} alt="Selected Option" className="w-full h-100 object-cover" />
+    <img src={src} alt="Selected Option" className="w-60 h-60 object-cover" />
     {selected && (
-      <div className="absolute inset-0 flex items-center justify-center ">
+      <div className="absolute inset-0 flex items-center justify-center bg-opacity-40">
         <img src={selectedIcon} alt="Check" className="w-11 h-11" />
       </div>
     )}
