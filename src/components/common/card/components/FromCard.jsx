4a1588926ed9ad745da7b08ef139ca5b26192b6b
ARON-Y <arar0811@gmail.com>
2025-02-10 23:05:56 +0900

    fix : css 수정

--- src/components/common/card/components/FromCard.jsx
+++ src/components/common/card/components/FromCard.jsx
@@ -32,10 +32,10 @@ function FromCard({
             </div>
             {isShowDeleteButton && <Button variant="icon" icon={trash} onClick={onDelete} />}
           </div>
-          <span className="text-18-regular mb-4 leading-7 tracking-[-0.01em] text-gray-600 flex-1 overflow-hidden line-clamp-4">
+          <span className="h-[106px] text-18-regular mb-4 leading-7 tracking-[-0.01em] text-gray-600 flex-1 overflow-hidden line-clamp-4">
             {messageContent}
           </span>
-          <p className="text-12-regular leading-4.5 tracking-[-0.005em] text-gray-400 absolute bottom-6 left-6">
+          <p className="text-12-regular leading-4.5 tracking-[-0.005em] text-gray-400">
             {formattedDate}
           </p>
         </article>
