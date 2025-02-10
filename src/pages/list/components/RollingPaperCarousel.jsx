00468b0b01286f3267bf37ea17cf54d350b1bbf9
hdh12191 <hdh12191@naver.com>
2025-02-10 20:45:34 +0900

    refactor : list page api 호출 구조 개선 및 이미지 프리로딩 함수 구현

--- src/pages/list/components/RollingPaperCarousel.jsx
+++ src/pages/list/components/RollingPaperCarousel.jsx
@@ -2,7 +2,15 @@ import CarouselTitle from './CarouselTitle';
 import CarouselContent from './CarouselContent';
 import CarouselNavigation from './CarouselNavigation';
 
-const RollingPaperCarousel = ({ title, papers, currentIndex, onNext, onPrev, itemsPerView }) => {
+const RollingPaperCarousel = ({
+  title,
+  papers,
+  currentIndex,
+  onNext,
+  onPrev,
+  itemsPerView,
+  profileImages,
+}) => {
   return (
     <div className="rolling-paper-carousel">
       <CarouselTitle title={title} />
@@ -13,7 +21,7 @@ const RollingPaperCarousel = ({ title, papers, currentIndex, onNext, onPrev, ite
         onPrev={onPrev}
         onNext={onNext}
       />
-      <CarouselContent currentIndex={currentIndex} papers={papers} />
+      <CarouselContent currentIndex={currentIndex} papers={papers} profileImages={profileImages} />
     </div>
   );
 };
